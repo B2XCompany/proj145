51cb513473cb91e907101afc1b52277d777d66ad
Bernardo Fraga <fragacostabe@gmail.com>
2024-04-14 14:28:21 -0300

    .

--- index.php
+++ index.php
@@ -18,45 +18,49 @@ if(!$__EMAIL__){
 </head>
 <body>
     <? if(requireLevel($__TYPE__, 1)){ ?>
-    <div id="adicionarProdutos">
-        <form action="javascript:void(0)">
-            <div class="input-div">
-                <p class="input-p">Nome</p>
-                <input type="text" id="nomereg" placeholder="Ex: Cadeira Giratória Ergonômica">
-            </div>
-            <div class="input-div">
-                <p class="input-p">Plaqueta / Lote</p>
-                <input type="text" id="plaquetareg" placeholder="Ex: 123456">
-            </div>
-            <div class="input-div">
-                <p class="input-p">Imobilizado</p>
-                <label class='toggle' for='imobilizadoreg'></label>
-                <input type="checkbox" id="imobilizadoreg">
-            </div>
-            <div class="input-div">
-                <p class="input-p">Entidade</p>
-                <select id='entidadereg'>
-                    <option value=''>Nenhuma</option>
-                    <option value='iel'>IEL</option>
-                    <option value='sesi'>SESI</option>
-                    <option value='senai'>SENAI</option>
-                    <option value='fiesc'>FIESC</option>
-                </select>
-            </div>
-            <div class="input-div">
-                <p class="input-p">Setor</p>
-                <select id="setorreg"></select>
-            </div>
-            <div class="input-div">
-                <p class="input-p">Descrição</p>
-                <input type="text" id="descricaoreg" placeholder="Ex: Cadeira giratória ergonômica com apoio lombar ajustável.">
-            </div>
-            <div class="input-div">
-                <p class="input-p">Quantidade</p>
-                <input type="number" id="quantidadereg" placeholder="Ex: 2">
-            </div>
-            <button onclick="addNewData('insertItem.php', {nome: nomereg.value, plaqueta: plaquetareg.value, imobilizado: imobilizadoreg.value, entidade: entidadereg.value, setor: setorreg.value, descricao: descricaoreg.value, quantidade: quantidadereg.value})">Cadastrar</button>
-        </form>
+    <div id="top">
+        <div id="adicionarProdutos">
+            <form action="javascript:void(0)">
+                <div class="input-div">
+                    <p class="input-p">Nome</p>
+                    <input type="text" id="nomereg" placeholder="Ex: Cadeira Giratória Ergonômica">
+                </div>
+                <div class="input-div">
+                    <p class="input-p">Plaqueta / Lote</p>
+                    <input type="text" id="plaquetareg" placeholder="Ex: 123456">
+                </div>
+                <div class="input-div">
+                    <p class="input-p">Imobilizado</p>
+                    <label class='toggle' for='imobilizadoreg'></label>
+                    <input type="checkbox" id="imobilizadoreg">
+                </div>
+                <div class="input-div">
+                    <p class="input-p">Entidade</p>
+                    <select id='entidadereg'>
+                        <option value=''>Nenhuma</option>
+                        <option value='iel'>IEL</option>
+                        <option value='sesi'>SESI</option>
+                        <option value='senai'>SENAI</option>
+                        <option value='fiesc'>FIESC</option>
+                    </select>
+                </div>
+                <div class="input-div">
+                    <p class="input-p">Setor</p>
+                    <select id="setorreg"></select>
+                </div>
+                <div class="input-div">
+                    <p class="input-p">Descrição</p>
+                    <input type="text" id="descricaoreg" placeholder="Ex: Cadeira giratória ergonômica com apoio lombar ajustável.">
+                </div>
+                <div class="input-div">
+                    <p class="input-p">Quantidade</p>
+                    <input type="number" id="quantidadereg" placeholder="Ex: 2">
+                </div>
+                <button onclick="addNewData('insertItem.php', {nome: nomereg.value, plaqueta: plaquetareg.value, imobilizado: imobilizadoreg.value, entidade: entidadereg.value, setor: setorreg.value, descricao: descricaoreg.value, quantidade: quantidadereg.value})">Cadastrar</button>
+            </form>
+        </div>
+        <div id="aprovarUsuarios"></div>
+        <div id="aprovarPedidos"></div>
     </div>
     <? } ?>
 
